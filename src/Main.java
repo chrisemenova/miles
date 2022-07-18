acdf62e52742538634b5ddc06a436b8e30fe3697
chrisemenova <chrisemenova88@gmail.com>
2022-07-18 16:24:43 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 8765;
+        int bonus = 20;
+        int miles = ticketPrice/bonus;
+
+        System.out.println("У Вас " + miles + " бонусных миль");
+    }
+}
